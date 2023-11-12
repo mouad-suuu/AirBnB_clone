c630400001d456ebd5c7824d043f3a18e5ae637f
mouad-suuu <moad.menniui@gmail.com>
2023-11-12 08:37:23 -0800

    amenity model

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+defines the state model
+'''
+from .base_model import BaseModel
+
+class Amenity(BaseModel):
+    '''
+    Blueprint for Amenity objects
+    '''
+    name = ""
